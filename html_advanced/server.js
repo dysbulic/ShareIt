a12f588f27e32b4ce029c5c9b1d2b2a930619ba0
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-09-11 21:13:23 +0200

    Removed DataChannel proxy server since Firefox 18 support PeerConnection

--- html_advanced/server.js
+++ html_advanced/server.js
@@ -1,6 +1,5 @@
 // SSL Certificates
-var fs        = require('fs')
-  , socket_io = require('socket.io');
+var fs = require('fs')
 
 var options = {key:  fs.readFileSync('../certs/privatekey.pem').toString(),
 			   cert: fs.readFileSync('../certs/certificate.pem').toString(),
@@ -51,74 +50,6 @@ wss.on('connection', function(socket)
     console.log("Connected socket.id: "+socket.id)
 })
 
-// DataChannel proxy server
-//var server = require('http').createServer().listen(PORT_PROXY);
-var server = require('https').createServer(options).listen(PORT_PROXY);
-var WebSocketServer = require('ws').Server
-var wss = new WebSocketServer({port: server})
-
-//Array to store connections
-wss.sockets = {}
-
-wss.on('connection', function(socket)
-{
-    function connect_to(socketId)
-    {
-        // Find peer on socket connected peers
-        var room = socket.rooms[socketId]
-
-        // Socket is already connected to the peer on a room
-        if(room != undefined)
-        {
-            socket.send(JSON.stringify(['connect_to.success', socketId, room]));
-            return
-        }
-
-        var peer = io.sockets.sockets[socketId]
-//        var peer = wss.sockets[socketId]
-
-        // Peer is not found, raise error
-        if(peer == undefiend)
-        {
-            socket.send(JSON.stringify(['connect_to.error', socketId]));
-            return
-        }
-
-        // Peer have been found, create a room and connect them
-    }
-
-    // Message received
-    socket.onmessage function(message)
-    {
-        console.log("socket.onmessage = '"+message.data+"'")
-        var args = JSON.parse(message.data)
-
-        var eventName = args[0]
-        var socketId  = args[1]
-
-        switch(eventName)
-        {
-            case 'connect_to':
-                connect_to(socketId)
-        }
-
-
-
-        var soc = io.sockets.sockets[socketId]
-//        var soc = wss.sockets[socketId]
-        if(soc)
-        {
-            args[1] = socket.id
-
-            soc.send(JSON.stringify(args));
-        }
-        else
-        {
-            socket.send(JSON.stringify([eventName+'.error', socketId]));
-            console.warn(eventName+': '+socket.id+' -> '+socketId);
-        }
-    }
-})
 
 // generate a 4 digit hex code randomly
 function S4() {
