8d1d9bcf23208c5f89838837352fd15620ac7139
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-09-28 19:20:25 +0200

    Changed parameters so socketId can be ommited on rooms

--- js/PeersManager.js
+++ js/PeersManager.js
@@ -25,7 +25,7 @@ function PeersManager()
             // Send offer to new PeerConnection
             var offer = pc.createOffer();
 
-            signaling.emit("connectTo", uid, offer.toSdp());
+            signaling.emit("connectTo", offer.toSdp(), uid);
 
             pc.setLocalDescription(pc.SDP_OFFER, offer);
         }
