3e813612e4b4d3365f90e09e6e55df3c45312be3
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-10-26 20:59:44 +0200

    Moved CompatibilityManager class to own file

--- js/index.js
+++ js/index.js
@@ -57,55 +57,6 @@ function load()
 }
 
 
-// Show an alert about the compatibility issues for the webapp on the browser
-function CompatibilityManager()
-{
-	var errors
-	var warnings
-
-	this.addError = function(component, msg)
-	{
-		errors = errors || {}
-		errors[component] = msg
-	}
-
-	this.addWarning = function(component, msg)
-	{
-		warnings = warnings || {}
-		warnings[component] = msg
-	}
-
-	this.show = function()
-	{
-		var msg = "ShareIt! will not work "
-
-		if(errors)
-		{
-	        msg += "on your browser because it doesn't meet the following requeriments:\n\n"
-            for(var key in errors)
-            	msg += key+': '+errors[key]+'\n';
-
-	        if(warnings)
-	        {
-	            msg += "\nAlso, it wouldn't work optimally because the following issues:\n\n"
-	            for(var key in warnings)
-	            	msg += key+': '+warnings[key]+'\n';
-	        }
-		}
-		else if(warnings)
-	        msg += "optimally on your browser because the following issues:\n\n"
-            for(var key in warnings)
-            	msg += key+': '+warnings[key]+'\n';
-
-		if(errors || warnings)
-		{
-			msg += "\nPlease upgrade to the latest version of Chrome/Chromium or Firefox."
-			alert(msg);
-		}
-	}
-}
-
-
 window.addEventListener("DOMContentLoaded", function()
 //window.addEventListener("load", function()
 {
