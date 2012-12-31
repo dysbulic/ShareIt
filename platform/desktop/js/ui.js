3bd9d64f4cb0b1559ae3b9b399d073dd333d6dd4
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-12-31 01:50:28 +0100

    Moved initialization of peers tabs to own class

--- platform/desktop/js/ui.js
+++ platform/desktop/js/ui.js
@@ -2,34 +2,6 @@ function UI()
 {
     EventTarget.call(this)
 
-    $("#tabs").tabs(
-    {
-        activate: function(event, ui)
-        {
-            $("#StartHere").remove()
-        },
-
-        active: false,
-        collapsible: true,
-        disabled: true
-    })
-
-    // close icon: removing the tab on click
-    $("#tabs span.ui-icon-closethick").live("click", function()
-    {
-        var index = $("#ui-corner-top", $("#tabs")).index($(this).parent());
-
-        // Remove the tab
-        var tab = $("#tabs").find(".ui-tabs-nav li:eq("+index+")").remove();
-        // Find the id of the associated panel
-        var panelId = tab.attr("aria-controls");
-        // Remove the panel
-        $("#"+panelId).remove();
-
-        // Refresh the tabs widget
-        $("tabs").tabs("refresh");
-    });
-
     var dialog_options =
     {
         autoOpen: false,
@@ -215,6 +187,9 @@ UI.prototype =
         tabSharing_update()
 
 
+        // Peers tabs
+        var tabs = new TabsPeers("tabs")
+
         /**
          * User initiated process to connect to a remote peer asking for the UID
          */
@@ -232,68 +207,8 @@ UI.prototype =
 	            // Create connection with the other peer
                 peersManager.connectTo(uid, function(channel)
                 {
-                    var tabs = $("#tabs")
-
-                    // Get index of the peer tab
-                    var index = tabs.find('table').index($('#tabs-'+uid))
-
-                    // Peer tab exists, open it
-                    if(index != -1)
-                        tabs.tabs("option", "active", index);
-
-                    // Peer tab don't exists, create it
-                    else
-                    {
-                        var tabPeer = new TabPeer(uid, self.preferencesDialogOpen,
-                        function(fileentry)
-                        {
-                            return function()
-                            {
-                                policy(function()
-                                {
-                                    // Begin transfer of file
-                                    peersManager._transferbegin(fileentry)
-
-                                    // Don't buble click event
-                                    return false;
-                                })
-                            }
-                        })
-
-                        peersManager.addEventListener("transfer.begin", function(event)
-                        {
-                            var fileentry = event.data[0]
-
-                            tabPeer.dispatchEvent({type: fileentry.hash+".begin"})
-                        })
-                        peersManager.addEventListener("transfer.update", function(event)
-                        {
-                            var fileentry = event.data[0]
-                            var value = event.data[1]
-
-                            tabPeer.dispatchEvent({type: fileentry.hash+".update",
-                                                   data: [value]})
-                        })
-                        peersManager.addEventListener("transfer.end", function(event)
-                        {
-                            var fileentry = event.data[0]
-
-                            tabPeer.dispatchEvent({type: fileentry.hash+".end"})
-                        })
-
-                        // Get notified when this channel files list is updated
-                        // and update the UI peer files table
-                        channel.addEventListener("fileslist._updated",
-                        function(event)
-                        {
-                            var fileslist = event.data[0]
-
-                            tabPeer.update(fileslist)
-                        })
-
-                        // Request the peer's files list
-                        channel.fileslist_query();
-                    }
+                    tabs.openOrCreate(uid, self.preferencesDialogOpen,
+                                      peersManager, channel)
                 },
 	            function(uid, peer, channel)
 	            {
