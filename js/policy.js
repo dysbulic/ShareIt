4737ae97b76b0c306027a93fea24db73fb0ee8de
Jesús Leganés Combarro <piranna@gmail.com>
2013-01-16 23:53:14 +0100

    Policy file loaded only one time

--- js/policy.js
+++ js/policy.js
@@ -4,66 +4,80 @@
  */
 function policy(onaccept, oncancel)
 {
-    // Exec 'onaccept' callback automatically if policy was accepted previously
-    if(onaccept && localStorage.policy_acepted)
+    function check()
     {
-        onaccept();
-        return
+        // Exec 'onaccept' callback automatically if policy was accepted previously
+        if(onaccept && localStorage.policy_acepted)
+        {
+            onaccept();
+            return
+        }
+
+        // Policy was not accepted previously
+        // or we are showing it ('onaccept' callback was not defined)
+        policy.dialog.dialog(
+        {
+            modal: true,
+            resizable: false,
+            width: 800,
+
+            buttons:
+            {
+                Cancel: function()
+                {
+                    $(this).dialog("close");
+
+                    // Policy not acepted, remove it
+                    localStorage.removeItem('policy_acepted')
+
+                    console.warn("Policy was NOT accepted")
+
+                    if(oncancel)
+                        oncancel();
+                },
+                Accept: function()
+                {
+                    $(this).dialog("close");
+
+                    // Policy acepted, set date and exec 'onaccept'
+                    localStorage.policy_acepted = (new Date()).getTime()
+
+                    console.warn("Policy was accepted")
+
+                    if(onaccept)
+                        onaccept();
+                }
+            }
+        });
     }
 
-    // Policy was not accepted previously
-    // or we are showing it (callback was not defined)
-    var http_request = new XMLHttpRequest();
-        http_request.open("GET", "../../policy.html");
-        http_request.onload = function()
-        {
-            if(this.status == 200)
+    // Policy text was loaded previously, check if it's accepted
+    if(policy.dialog)
+        check()
+
+    // Checking the policy the first time on this session, load the policy text
+    else
+    {
+        var http_request = new XMLHttpRequest();
+            http_request.open("GET", "../../policy.html");
+            http_request.onload = function()
             {
-                var policy = $("#dialog-policy")
-                    policy.html(http_request.response)
-
-                policy.dialog(
+                // Policy text loaded successfully, fill the dialog and check it
+                if(this.status == 200)
                 {
-                    modal: true,
-                    resizable: false,
-                    width: 800,
+                    policy.dialog = $("#dialog-policy")
+                    policy.dialog.html(http_request.response)
 
-                    buttons:
-                    {
-                        Cancel: function()
-                        {
-                            $(this).dialog("close");
+                    check()
+                }
 
-                            // Policy not acepted, remove it
-                            localStorage.removeItem('policy_acepted')
-
-                            console.warn("Policy was not accepted")
-
-                            if(oncancel)
-                                oncancel();
-                        },
-                        Accept: function()
-                        {
-                            $(this).dialog("close");
-
-                            // Policy acepted, set date and exec 'onaccept'
-                            localStorage.policy_acepted = (new Date()).getTime()
-
-                            console.warn("Policy was accepted")
-
-                            if(onaccept)
-                                onaccept();
-                        }
-                    }
-                });
-            }
-
-            else
+                else
+                    console.error("There was an error loading the Usage Policy")
+            };
+            http_request.onerror = function()
+            {
                 console.error("There was an error loading the Usage Policy")
-        };
-        http_request.onerror = function()
-        {
-            console.error("There was an error loading the Usage Policy")
-        }
-        http_request.send();
+            }
+            http_request.send();
+    }
 }
